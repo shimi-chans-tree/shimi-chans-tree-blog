253d64ea184c8db98655580612b668abc70dceb9
koji <begin.1219@icloud.com>
2022-06-23 07:56:35 +0900

    HomeTemplateとstyleを追加

---
+++ src/components/pages/HomeTemplate/index.tsx
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react';
+import BasePostPageLayout from '../../Layouts/BasePostPageLayout';
+import { BlogItem } from '../../common/molecules/BlogItem';
+import AppContext from '../../../contexts/AppContext';
+import { BlogItemResponsive } from '../../common/molecules/BlogItemResponsive';
+/* styles */
+import styles from './styles.module.scss';
+
+/* types */
+import { BlogItemType } from '../../../types/Blog';
+
+const HomeTemplate: React.FC = () => {
+  const { state } = useContext(AppContext);
+  const { blogList } = state.blogItem;
+
+  return (
+    <BasePostPageLayout>
+      <div className={styles.blogItem}>
+        {blogList &&
+          blogList.length > 0 &&
+          blogList.map((blogItem: BlogItemType, index: number) => (
+            <BlogItem key={`${blogItem.id}_${index}`} blogItem={blogItem} />
+          ))}
+      </div>
+      {/* ブログ記事一覧表示 レスポンシブ*/}
+      <div className={styles.blogItem__responsive}>
+        {blogList &&
+          blogList.length > 0 &&
+          blogList.map((blogItem: BlogItemType, index: number) => (
+            // <></>
+            <BlogItemResponsive
+              key={`${blogItem.id}_${index}`}
+              blogItem={blogItem}
+            />
+          ))}
+      </div>
+    </BasePostPageLayout>
+  );
+};
+
+export default HomeTemplate;
