c66259e1b1153b7a70f5669120fb719ea97e9b69
koji <begin.1219@icloud.com>
2022-06-25 06:44:37 +0900

    シンタックスハイライトのコンポーネントを追加

---
+++ src/components/common/molecules/HighlightBody/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import 'highlight.js/styles/shades-of-purple.css';
+/* styles */
+import styles from './styles.module.scss';
+
+/**
+ * props
+ */
+type Props = {
+  highlightedBody: string;
+};
+
+/**
+ * HighlightBody
+ * @param {Props} props
+ * @returns
+ */
+export const HighlightBody: React.FC<Props> = (props: Props) => {
+  /* props */
+  const { highlightedBody } = props;
+
+  return (
+    <div
+      className={styles.contents}
+      dangerouslySetInnerHTML={{
+        __html: highlightedBody,
+      }}
+    />
+  );
+};
