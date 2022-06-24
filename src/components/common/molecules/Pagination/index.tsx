d1fcf7d084dfb5ddc05943503266b02ca93debc6
koji <begin.1219@icloud.com>
2022-06-25 06:53:58 +0900

    ページネーションのコンポーネントを追加

---
+++ src/components/common/molecules/Pagination/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'next/link';
+/* hooks */
+import { usePagination } from '../../../../hooks/usePagination';
+/* constants */
+import { BLOG_SHOW_COUNT } from '../../../../constants/config';
+/* styles */
+import styles from './styles.module.scss';
+
+/**
+ * props
+ */
+type Props = {
+  totalCount: number;
+  link: string;
+};
+
+export const Pagination: React.FC<Props> = (props: Props) => {
+  /* props */
+  const { totalCount, link } = props;
+  /* hooks */
+  const [{ currentPage }, { createPageRange }] = usePagination();
+
+  return (
+    <ul className={styles.container}>
+      {totalCount !== 0 &&
+        createPageRange(1, Math.ceil(totalCount / BLOG_SHOW_COUNT)).map(
+          (number, index) => (
+            <li className={styles.iconArea} key={index}>
+              <Link href={`${link}${number}`}>
+                <span
+                  className={
+                    currentPage !== number ? styles.icon : styles.currentIcon
+                  }
+                >
+                  {number}
+                </span>
+              </Link>
+            </li>
+          )
+        )}
+    </ul>
+  );
+};
