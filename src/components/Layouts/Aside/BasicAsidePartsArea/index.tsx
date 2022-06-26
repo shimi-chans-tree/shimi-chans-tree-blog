4aa949e1e41a860027c8fcb0a2df358a25762e09
koji <begin.1219@icloud.com>
2022-06-26 11:22:43 +0900

    sideエリアのベースファイルを追加

---
+++ src/components/Layouts/Aside/BasicAsidePartsArea/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+/* styles */
+import styles from './styles.module.scss';
+
+/**
+ * props
+ */
+type Props = {
+  children: React.ReactNode;
+  title: string;
+};
+
+export const BasicAsidePartsArea: React.FC<Props> = (props: Props) => {
+  /* props */
+  const { children, title } = props;
+
+  return (
+    <>
+      <h6 className={styles.title}>{title}</h6>
+      <div className={styles.bar} />
+      <div className={styles.field}>{children}</div>
+    </>
+  );
+};
