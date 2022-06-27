f2974f57ea85c40a32085c47a080cb5d4d164ac9
koji <begin.1219@icloud.com>
2022-06-27 10:48:39 +0900

    e2eテストを追加

--- cypress/e2e/Home.cy.ts
+++ cypress/e2e/Home.cy.ts
@@ -1,4 +1,4 @@
-it('ルートパスに訪問できるか', () => {
+it('ルートパスに遷移できるか', () => {
   cy.visit('/');
 });
 
@@ -7,5 +7,13 @@ describe('Header Test', () => {
     cy.visit('/');
     cy.get('[data-test-id="home-header-link"]').click();
     cy.url().should('include', '/');
+  });
+});
+
+describe('Search Test', () => {
+  it('検索フォームに入力しEnterを押すことでsearchページに遷移すること', () => {
+    cy.visit('/');
+    cy.get('[data-test-id="search-link"]').eq(1).type('Hello{enter}');
+    cy.url().should('include', '/search');
   });
 });
